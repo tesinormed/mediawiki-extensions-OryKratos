9f580a3c972ea64d968fc87ce0363d34f447b6ba
tesinormed <tesinormed@5505.industries>
2025-08-14 22:20:38 -0700

    version 0.9.0

--- src/OryKratos.php
+++ src/OryKratos.php
@@ -4,8 +4,13 @@ namespace MediaWiki\Extension\OryKratos;
 
 use GuzzleHttp\Client;
 use MediaWiki\Auth\AuthManager;
+use MediaWiki\Extension\PluggableAuth\BackchannelLogoutAwarePlugin;
 use MediaWiki\Extension\PluggableAuth\PluggableAuth;
+use MediaWiki\Rest\RequestInterface;
+use MediaWiki\Rest\ResponseInterface;
+use MediaWiki\Session\SessionManagerInterface;
 use MediaWiki\SpecialPage\SpecialPage;
+use MediaWiki\User\UserFactory;
 use MediaWiki\User\UserIdentity;
 use MediaWiki\User\UserIdentityLookup;
 use Ory\Kratos\Client\Api\FrontendApi;
@@ -15,23 +20,27 @@ use Ory\Kratos\Client\Configuration;
 use RuntimeException;
 use Wikimedia\Rdbms\IConnectionProvider;
 
-class OryKratos extends PluggableAuth {
+class OryKratos extends PluggableAuth implements BackchannelLogoutAwarePlugin {
 	private const IDENTITY_ID_SESSION_KEY = 'OryKratosIdentityId';
 
 	private AuthManager $authManager;
 	private IConnectionProvider $dbProvider;
 	private UserIdentityLookup $userIdentityLookup;
+	private UserFactory $userFactory;
 	private FrontendApi $frontendApi;
 	private IdentityApi $identityApi;
+	private ?string $backchannelLogoutToken = null;
 
 	public function __construct(
 		AuthManager $authManager,
 		IConnectionProvider $dbProvider,
 		UserIdentityLookup $userIdentityLookup,
+		UserFactory $userFactory,
 	) {
 		$this->authManager = $authManager;
 		$this->dbProvider = $dbProvider;
 		$this->userIdentityLookup = $userIdentityLookup;
+		$this->userFactory = $userFactory;
 	}
 
 	/** @inheritDoc */
@@ -40,21 +49,21 @@ class OryKratos extends PluggableAuth {
 
 		if ( !$this->getData()->has( 'publicHost' ) ) {
 			throw new RuntimeException( '"publicHost" required in "data" block' );
+		} elseif ( !$this->getData()->has( 'adminHost' ) ) {
+			throw new RuntimeException( '"adminHost" required in "data" block' );
 		}
 
 		$this->frontendApi = new FrontendApi(
 			new Client(),
 			( new Configuration() )->setHost( $this->getData()->get( 'publicHost' ) )
 		);
-
-		if ( !$this->getData()->has( 'adminHost' ) ) {
-			throw new RuntimeException( '"adminHost" required in "data" block' );
-		}
-
 		$this->identityApi = new IdentityApi(
 			new Client(),
 			( new Configuration() )->setHost( $this->getData()->get( 'adminHost' ) )
 		);
+		if ( $this->getData()->has( 'backchannelLogoutToken' ) ) {
+			$this->backchannelLogoutToken = $this->getData()->get( 'backchannelLogoutToken' );
+		}
 	}
 
 	/** @inheritDoc */
@@ -87,30 +96,20 @@ class OryKratos extends PluggableAuth {
 			}
 		}
 
-		$identityId = $session->getIdentity()->getId();
+		$identity = $session->getIdentity();
 		// save the identity ID for later
-		$this->authManager->setAuthenticationSessionData( self::IDENTITY_ID_SESSION_KEY, $identityId );
+		$this->authManager->setAuthenticationSessionData( self::IDENTITY_ID_SESSION_KEY, $identity->getId() );
 
 		// grab the identity traits
-		$username = $session->getIdentity()->getTraits()->username;
-		$email = $session->getIdentity()->getTraits()->email;
-		if ( property_exists( $session->getIdentity()->getTraits(), 'name' ) ) {
-			$realname = $session->getIdentity()->getTraits()->name;
+		$identityTraits = $identity->getTraits();
+		$username = $identityTraits->username;
+		$email = $identityTraits->email;
+		if ( property_exists( $identityTraits, 'name' ) ) {
+			$realname = $identityTraits->name;
 		}
 
 		// see if there's a mapping for this identity
-		$dbr = $this->dbProvider->getReplicaDatabase();
-		$field = $dbr->newSelectQueryBuilder()
-			->select( 'user_id' )
-			->from( 'user' )
-			->join( 'ory_kratos', conds: 'user_id=kratos_user' )
-			->where( [
-				'kratos_id' => $identityId,
-				'kratos_host' => $publicHost
-			] )
-			->useIndex( 'ory_kratos_id' )
-			->caller( __METHOD__ )
-			->fetchField();
+		$field = $this->findUserByIdentityId( $identity->getId(), $publicHost );
 
 		if ( $field !== false ) {
 			// identity already mapped to MediaWiki user
@@ -132,6 +131,19 @@ class OryKratos extends PluggableAuth {
 		return true;
 	}
 
+	private function findUserByIdentityId( string $identityId, string $publicHost ): mixed {
+		return $this->dbProvider->getReplicaDatabase()->newSelectQueryBuilder()
+			->select( 'user_id' )
+			->from( 'user' )
+			->join( 'ory_kratos', conds: 'user_id=kratos_user' )
+			->where( [
+				'kratos_id' => $identityId,
+				'kratos_host' => $publicHost
+			] )
+			->useIndex( 'ory_kratos_id' )
+			->caller( __METHOD__ )->fetchField();
+	}
+
 	/** @inheritDoc */
 	public function shouldOverrideDefaultLogout(): bool {
 		return true;
@@ -139,28 +151,16 @@ class OryKratos extends PluggableAuth {
 
 	/** @inheritDoc */
 	public function deauthenticate( UserIdentity &$user ): void {
-		self::logoutAllSessions( $user, $this->dbProvider, $this->frontendApi, $this->identityApi );
-	}
-
-	public static function logoutAllSessions(
-		UserIdentity $user,
-		IConnectionProvider $dbProvider,
-		FrontendApi $frontendApi,
-		IdentityApi $identityApi
-	): void {
 		// see if there's a mapping for this MediaWiki user
-		$dbr = $dbProvider->getReplicaDatabase();
-		$identityId = $dbr->newSelectQueryBuilder()
+		$identityId = $this->dbProvider->getReplicaDatabase()->newSelectQueryBuilder()
 			->select( 'kratos_id' )
 			->from( 'ory_kratos' )
 			->where( [
 				'kratos_user' => $user->getId(),
-				'kratos_host' => $frontendApi->getConfig()->getHost()
+				'kratos_host' => $this->frontendApi->getConfig()->getHost()
 			] )
 			->useIndex( 'ory_kratos_id' )
-			->caller( __METHOD__ )
-			->fetchField();
-
+			->caller( __METHOD__ )->fetchField();
 		if ( $identityId === false ) {
 			// no mapping
 			return;
@@ -168,7 +168,7 @@ class OryKratos extends PluggableAuth {
 
 		try {
 			// logout all sessions for the mapped identity
-			$identityApi->deleteIdentitySessions( $identityId );
+			$this->identityApi->deleteIdentitySessions( $identityId );
 		} catch ( ApiException $exception ) {
 			wfLogWarning( $exception->getMessage() );
 		}
@@ -176,18 +176,73 @@ class OryKratos extends PluggableAuth {
 
 	/** @inheritDoc */
 	public function saveExtraAttributes( int $id ): void {
-		// retrieve the identity ID saved earlier
-		$identityId = $this->authManager->getAuthenticationSessionData( self::IDENTITY_ID_SESSION_KEY );
-
-		$dbw = $this->dbProvider->getPrimaryDatabase();
-		$dbw->newInsertQueryBuilder()
+		$this->dbProvider->getPrimaryDatabase()->newInsertQueryBuilder()
 			->insertInto( 'ory_kratos' )
 			->row( [
 				'kratos_user' => $id,
-				'kratos_id' => $identityId,
+				// retrieve the identity ID saved earlier
+				'kratos_id' => $this->authManager->getAuthenticationSessionData( self::IDENTITY_ID_SESSION_KEY ),
 				'kratos_host' => $this->frontendApi->getConfig()->getHost()
 			] )
-			->caller( __METHOD__ )
-			->execute();
+			->caller( __METHOD__ )->execute();
+	}
+
+	/** @inheritDoc */
+	public function canHandle( RequestInterface $request ): bool {
+		if ( $request->getMethod() !== 'POST'
+			|| !$request->hasBody()
+			|| $request->getBodyType() !== 'application/json'
+		) {
+			return false;
+		}
+
+		$authorizationHeaders = $request->getHeader( 'Authorization' );
+		if ( empty( $authorizationHeaders ) ) {
+			// no Authorization header
+			return false;
+		}
+
+		foreach ( $authorizationHeaders as $authorizationHeader ) {
+			if ( !str_starts_with( $authorizationHeader, 'Bearer ' ) ) {
+				// not using Bearer
+				return false;
+			}
+
+			$token = str_replace( 'Bearer ', '', $authorizationHeader );
+			if ( $token === $this->backchannelLogoutToken ) {
+				// valid Bearer token
+				return true;
+			}
+		}
+
+		// invalid Bearer token(s)
+		return false;
+	}
+
+	/** @inheritDoc */
+	public function performBackchannelLogout(
+		RequestInterface $request,
+		ResponseInterface $response,
+		SessionManagerInterface $sessionManager
+	): void {
+		$parsedBody = json_decode( (string)$request->getBody(), associative: true );
+		if ( !is_array( $parsedBody ) ) {
+			$response->setStatus( 400 );
+			return;
+		}
+
+		$userId = $this->findUserByIdentityId( $parsedBody['identity_id'], $this->frontendApi->getConfig()->getHost() );
+		if ( $userId === false ) {
+			$response->setStatus( 400 );
+			return;
+		}
+
+		$user = $this->userFactory->newFromUserIdentity( $this->userIdentityLookup->getUserIdentityByName( $userId ) );
+		$this->getLogger()->debug( 'Logging out {username} ({userid})', [
+			'username' => $user->getName(),
+			'userid' => $user->getId()
+		] );
+		$sessionManager->invalidateSessionsForUser( $user );
+		$response->setStatus( 200 );
 	}
 }
